91b2c0a034630cb53bb019a6adb555c5937fdf50
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-02-05 13:02:06 +0100

    Past inputting names (before splitting in separation of concerns

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  
   get '/' do
+    @player_1_name = params['player_1_name']
+    @player_2_name = params['player_2_name']
     erb(:index)
   end
+
+  # get '/names' do
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -7,4 +7,13 @@ feature 'Enter names' do
 		click_button 'Submit'
 		expect(page).to have_content 'Bjoern vs. Elaine'
 	end
+
+  scenario 'submitting names' do
+    visit('/')
+    fill_in(:player_1_name, with: 'Mark')
+    fill_in(:player_2_name, with: 'Bjoern')
+    click_button 'Submit'
+    expect(page).not_to have_content 'Bjoern vs. Elaine'
+    expect(page).to have_content 'Mark vs. Bjoern'
+  end
 end
